d1531f5921140d50952db13eecf88ccd79405885
李太新 <toonew@danwi.me>
2016-07-04 17:37:32 +0800

    service 层 实现，

---
+++ app/hero.service.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by Rain on 2016/7/4.
+ */
+import {Injectable} from '@angular/core';
+
+import {Hero} from './hero';
+import {HEROES}  from './mock-heroes'
+
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        // return Promise.resolve(HEROES)
+        return new Promise((resolve, reject)=> {
+            setTimeout(()=> {
+                resolve(HEROES)
+            }, 3000)
+        });
+    }
+}
---
+++ app/hero.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Rain on 2016/7/4.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
+
+
