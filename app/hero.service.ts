8db842b2ff56b3ed270927e8f3dca104ca501d3f
李太新 <toonew@danwi.me>
2016-07-05 16:17:24 +0800

    routing 层 实现

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,16 +4,21 @@
 import {Injectable} from '@angular/core';
 
 import {Hero} from './hero';
-import {HEROES}  from './mock-heroes'
+import {HEROES}  from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+    getHero(id:number) {
+        return this.getHeroes()
+            .then(heroes => heroes.filter(hero => hero.id === id)[0]);
+    }
+
     getHeroes() {
         // return Promise.resolve(HEROES)
         return new Promise<Hero[]>((resolve, reject)=> {
             setTimeout(()=> {
                 resolve(HEROES)
-            }, 3000)
+            }, 1000)
         });
     }
 }
