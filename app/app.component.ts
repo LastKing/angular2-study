ed1600f05a5fe4eba8b5240c32d86a50647b0a58
李太新 <toonew@danwi.me>
2016-09-28 00:34:46 +0800

    app 只是基本样式模块和所有的模块集成模块倒入点，还有rxjs 操作符的倒入

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,25 +2,20 @@
  * Created by Rain on 2016/6/3.
  */
 import {Component} from '@angular/core'
-import {ROUTER_DIRECTIVES} from '@angular/router';
-
-import {HeroService} from './hero.service'
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
-            <a [routerLink]="['/heroes']"  routerLinkActive="active">Heroes</a>
-        </nav>
-       <router-outlet></router-outlet>
-    `,
     styleUrls: ['app/app.component.css'],
-    directives: [ROUTER_DIRECTIVES],
-    providers: [HeroService]
+    template: `
+     <h1>{{title}}</h1>
+     <nav>
+     <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+     <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+     </nav>
+     <router-outlet></router-outlet>
+   `
 })
 
 export class AppComponent {
-    title:'Tour of heroes'
+    title = 'Tour of Heroes';
 }
---
+++ app/app.routing.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by Rain on 2016/7/5.
+ */
+import {ModuleWithProviders}  from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {HeroesComponent}  from './heroes.component';
+import {DashboardComponent}  from './dashboard.component';
+import {HeroDetailComponent} from "./hero-detail.component";
+
+const appRoutes: Routes = [
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+
+    {path: 'heroes', component: HeroesComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'detail/:id', component: HeroDetailComponent}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+
