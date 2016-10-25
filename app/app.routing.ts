95c1966e96a3536855b5376a6755d4796adb08d6
李太新 <toonew@danwi.me>
2016-10-25 17:53:57 +0800

    归并错误修复

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 
   {path: 'heroes', component: HeroesComponent},
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'detail/:id', component: HeroDetailComponent}
+  {path: 'detail/:id', component: HeroDetailComponent},
   {path: 'test', component: TestComponent}
 ];
 
